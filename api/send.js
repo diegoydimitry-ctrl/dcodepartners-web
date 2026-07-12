e73c349dfeffef2903e44b3fb51c240480aa7c0d
diegoydimitry-ctrl <diegoydimitry@gmail.com>
2026-07-12 09:49:27 +0200

    Update send.js

--- api/send.js
+++ api/send.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     const { nombre, empresa, email, telefono, mensaje } = req.body;
 
     await resend.emails.send({
-      from: "D-Code Partners <onboarding@resend.dev>",
+      from: "D-Code Partners <contact@dcodepartners.com>",
       to: ["dcodedepartment@gmail.com"],
       subject: `Nueva solicitud de ${nombre}`,
       html: `
